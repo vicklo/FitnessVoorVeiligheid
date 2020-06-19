32f1e53c5a4d080fc00df323914ae31abc0da3f6
menno van cranenbroek <ps197136@summacollege.nl>
2020-06-19 11:50:23 +0200

    docentprofiel werkende

--- components/docentprofiel.js
+++ components/docentprofiel.js
@@ -12,18 +12,15 @@ import ImgToBase64 from 'react-native-image-base64';
     super(props);
     this.state = {
         isloading:false,
-        klassen:null,
-        klas:""
+        
           };
   }
 
   componentDidMount =  async() => 
   {  
-
-      
+    await fetch(ipadress + 'docenten')
+      .then(response => response.json())  
   }
-  
-
   render() {
       if(this.state.isloading)
       return (
@@ -37,7 +34,41 @@ import ImgToBase64 from 'react-native-image-base64';
     else
         return(
             <ScrollView>
+                <View style={{width:300,height:100,position:"absolute",transform: [{ rotate: "45deg" }],backgroundColor:"#d80399",marginLeft:Dimensions.get('window').width - 180,shadowColor:"#000",shadowOffset:{width:0,height:2},shadowOpacity:0.4}}></View>
+              <View style={{width:300,height:100,position:"absolute",transform: [{ rotate: "45deg" }],backgroundColor:"#291876",marginLeft:Dimensions.get('window').width - 120,shadowColor:"#000",shadowOffset:{width:0,height:2},shadowOpacity:0.4}}></View>
+              <View style={styles.container}>
+              <Image style={{margin:5,borderRadius:75,width:150,height:150,borderWidth:2,borderColor:"#d80399",marginBottom:20}} onLoad={this.getbase64} source={{uri:loggedinuser.foto}}/>
+              <View style={{alignItems:"center",backgroundColor:"#291876",marginTop:20,width:Dimensions.get('window').width,shadowColor:"#000",shadowOffset:{width:0,height:2},shadowOpacity:0.4}}>
+                <View style={{flexDirection: "row",alignItems:"center",width:Dimensions.get('window').width}}>
+                  <Text style={{color:"white",textAlign:"left",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>username:</Text>
+                  <Text style={{color:"white",textAlign:"right",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>{loggedinuser.username}</Text>
+                </View>
+                <View style={{height:1,width: Dimensions.get('window').width - 15,backgroundColor:"#d80399",alignSelf:"center" ,borderRadius:2 }}></View>
+                  
+                  <View style={{flexDirection: "row",alignItems:"center",width:Dimensions.get('window').width}}>
+                    <Text style={{color:"white",textAlign:"left",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>Naam:</Text>
+                    <Text style={{color:"white",textAlign:"right",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>{loggedinuser.voornaam} {loggedinuser.achternaam}</Text>
+                  </View> 
+                  <View style={{height:1,width: Dimensions.get('window').width - 15,backgroundColor:"#d80399",alignSelf:"center" ,borderRadius:2 }}></View>
 
+                  <View style={{flexDirection: "row",alignItems:"center",width:Dimensions.get('window').width}}>
+                    <Text style={{color:"white",textAlign:"left",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>e-mail:</Text>
+                    <Text style={{color:"white",textAlign:"right",width:Dimensions.get('window').width / 2 - 20,margin:10,fontSize:18}}>{loggedinuser.docentmail}</Text>
+                  </View> 
+                  <View style={{height:1,width: Dimensions.get('window').width - 15,backgroundColor:"#d80399",alignSelf:"center" ,borderRadius:2 }}></View>
+                </View>
+                <Button onPress={() =>  this.props.navigation.navigate('docentprofielwijzig', { userid: this.state.userid })} style={{padding:5,margin:15,width:200,backgroundColor:"#291876",borderRadius:10,shadowColor:"#000",shadowOffset:{width:0,height:2},shadowOpacity:0.4}}>
+                    <Text style={{color:"white"}}>wijzig</Text>
+                </Button> 
+                <Button onPress={() => NativeModules.DevSettings.reload()} style={{padding:5,margin:15,width:200,backgroundColor:"#291876",borderRadius:10,shadowColor:"#000",shadowOffset:{width:0,height:2},shadowOpacity:0.4}}>
+                    <Text style={{color:"white"}}>Log uit</Text>
+                </Button>
+              </View>
+              <View style={{alignSelf:"flex-end",marginRight:Dimensions.get('window').width + 60,marginTop:-20}}>
+              <View style={{width:300,height:100,position:"absolute",transform: [{ rotate: "45deg" }],backgroundColor:"#d80399",marginLeft:-40,shadowColor:"#000",shadowOffset:{width:0,height:-2},shadowOpacity:0.4}}></View>
+              <View style={{width:300,height:100,position:"absolute",transform: [{ rotate: "45deg" }],backgroundColor:"#291876",marginLeft:-100,shadowColor:"#000",shadowOffset:{width:0,height:-2},shadowOpacity:0.4}}></View>
+
+              </View>
           </ScrollView>
         );
   }
